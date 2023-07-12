f1f128cfbcf74418f78cd7584415089cc7c3f4ed
salesof7 <jpsaless2002@gmail.com>
2023-07-12 00:28:06 -0300

    feat: create producer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	deliveryChan := make(chan kafka.Event)
+	producer := NewKafkaProducer()
+	Publish("transferiu", "teste", producer, []byte("transferencia"), deliveryChan)
+
+	go DeliveryReport(deliveryChan)
+	producer.Flush(5000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers":   "go-kafka-kafka-1:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",
+		"enable.idempotence":  "true",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
+	message := &kafka.Message{
+		Value: []byte(msg),
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: kafka.PartitionAny,
+		},
+		Key: key,
+	}
+	err := producer.Produce(message, deliveryChan)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Erro ao enviar a mensagem!")
+			} else {
+				fmt.Println("Mensage enviada: ", ev.TopicPartition)
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/salesof7/go_kafka-fs
 
 go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
